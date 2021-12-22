77a007b86fbdfc6cd9ec4b4612cbcf72ecccb510
PcBoyarin <92618851+SeemerGG@users.noreply.github.com>
2021-12-22 21:45:26 +0300

    1)2)3)-

--- DmLab19.1/DmLab19.1/Program.cs
+++ DmLab19.1/DmLab19.1/Program.cs
@@ -3,69 +3,152 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace DmLab19._1
 {
     class Program
     {
-        
-        public static bool HasNext(List<char> word,List<char> alf)
+        class Alfavit
         {
-            bool f = false;
-            for(int i = 0; i < word.Count; i++)
+            protected List<char> alf = new List<char>();
+            public Alfavit(string s)
             {
-                if (word[i] != alf[alf.Count - 1])
+                foreach (char b in s)
+                    if(b!=' ')
+                        alf.Add(b);
+            }
+
+        }
+        
+        class Object : Alfavit
+        {
+            List<char> word = new List<char>();
+            public Object(string s) : base(s) { }
+            
+            bool HasNext()
+            {
+                bool f = false;
+                for(int i = 0; i < word.Count;i++)
                 {
-                    f = true;
-                    break;
+                    if(word[i]!=alf[alf.Count-1])
+                    {
+                        f = true;
+                        break;
+                    }
+                }
+                return f;
+            }
+
+            void NextWord()
+            {
+                int i = word.Count - 1;
+                while (word[i] == alf[alf.Count - 1])
+                    i--;
+                word[i] = alf[alf.IndexOf(word[i]) + 1];
+                int j = i + 1;
+                while (j < word.Count)
+                {
+                    word[j] = alf[0];
+                    j++;
                 }
             }
-                return f;
-        }
 
-        public static void NextWord(ref List<char> word,List<char> alf)
-        {
-            int i = word.Count-1;
-            while (word[i] == alf[alf.Count - 1])
-                i--;
-            word[i] = alf[alf.IndexOf(word[i]) + 1];
-            int j =i+1;
-            while (j < word.Count)
+            void Vivod(StreamWriter fsr)
             {
-                word[j] = alf[0];
-                j++;
-            } 
-        }
+                foreach (char b in word)
+                    fsr.Write(b);
+                fsr.WriteLine();
+            }
 
-        public static void Vivod(List<char> word)
-        {
-            foreach (char b in word)
-                Console.Write(b);
-            Console.WriteLine();
+            void Swap(int i, int j)
+            {
+                char b = word[i];
+                word[i] = word[j];
+                word[j] = b;
+            }
+
+            public void SuchitaniyaWithPovtoreniyami(int k)
+            {
+                StreamWriter fsr = new StreamWriter(@"C:\Users\PcBoyarin\Desktop\GayDev\DmLab21-24\DmLab19.1\result\SWP.txt");
+                for (int i = 0; i < k; i++)
+                {
+                    word.Add(alf[0]);
+                }
+                Vivod(fsr);
+                while (HasNext())
+                {
+                    NextWord();
+                    Vivod(fsr);
+                }
+                fsr.Close();
+            }
+            
+            public void Perestanovki()
+            {
+                StreamWriter fsr = new StreamWriter(@"C:\Users\PcBoyarin\Desktop\GayDev\DmLab21-24\DmLab19.1\result\P.txt");
+                word = alf.GetRange(0, alf.Count);
+                Vivod(fsr);
+                while (true)
+                {
+                    int i = word.Count - 2;
+                    while (i != -1 && alf.IndexOf(word[i]) > alf.IndexOf(word[i + 1])) i--;
+                    if (i == -1)
+                        break;
+                    int j = word.Count - 1;
+                    while (alf.IndexOf(word[i]) > alf.IndexOf(word[j])) j--;
+                    Swap(i, j);
+                    int l = i + 1, r = word.Count - 1;
+                    while (l < r)
+                        Swap(l++, r--);
+                    Vivod(fsr);
+                }
+                fsr.Close();
+            }
+            ///
+            public void RazmeshPoK(int k)
+            {
+                StreamWriter fsr = new StreamWriter(@"C:\Users\PcBoyarin\Desktop\GayDev\DmLab21-24\DmLab19.1\result\R.txt");
+                word.Clear();
+                for (int i = 0; i < k; i++)
+                    word.Add(alf[i]);
+                Vivod(fsr);
+                bool f = true;
+                while (f)
+                {
+                    int i;
+                    do  
+                    {
+                        i = alf.Count - 1;
+                        while (i != -1 && alf.IndexOf(word[i]) > alf.IndexOf(word[i + 1])) i--;
+                        if (i == -1)
+                        { 
+                            f = false;
+                            break;
+                        }
+                        int j = alf.Count - 1;
+                        while (alf.IndexOf(word[i]) > alf.IndexOf(word[j])) j--;
+                        Swap(i, j);
+                        int l = i + 1, r = alf.Count - 1; 
+                        while (l < r)
+                            Swap(l++, r--);
+                    } while (i > k - 1);
+                    Vivod(fsr);
+                }
+                fsr.Close();
+            }
+            ///
         }
+        
         static void Main(string[] args)
         {
-            List<char> alf = new List<char>();
             Console.Write("Введите символы алфавита через пробел:");
-            List<char> word = new List<char>();
-            foreach (char b in Console.ReadLine())
-            {
-                if (b != ' ')
-                    alf.Add(b);
-            }
+            Object obj = new Object(Console.ReadLine());
             Console.Write("Введите число k:");
             int k = Convert.ToInt32(Console.ReadLine());
-            for(int i = 0; i < k; i++)
-            {
-                word.Add(alf[0]);
-            }
-            Vivod(word);
-            while (HasNext(word, alf))
-            {
-                
-                NextWord(ref word, alf);
-                Vivod(word);
-            } 
+            obj.SuchitaniyaWithPovtoreniyami(k);
+            obj.Perestanovki();
+            obj.RazmeshPoK(k);
             Console.ReadKey();
         }
     }
