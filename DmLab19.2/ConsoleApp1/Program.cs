ca5be0e13c757fa26cbff266d61bc8c970fa4af9
PcBoyarin <92618851+SeemerGG@users.noreply.github.com>
2021-12-24 20:31:39 +0300

    Вторая задача

--- DmLab19.2/ConsoleApp1/Program.cs
+++ DmLab19.2/ConsoleApp1/Program.cs
@@ -45,7 +45,24 @@ namespace DmLab19._2
                     return false;
             }
 
-            public void zad1()
+            bool BezPiAA()
+            {
+                int kol = 0;
+                for (int i = 0; i < 5; i++)
+                    if (word[i] == 'a')
+                        kol++;
+                if (kol == 2)
+                {
+                    for(int i=0;i<5;i++)
+                        for(int j=0;j<5;j++)
+                            if(i!=j && word[i] != 'a' && word[i] == word[j])
+                                return false;
+                    return true;
+                }
+                else
+                    return false;
+            }
+            public void Zad1()
             {
                 StreamWriter fsr = new StreamWriter(@"C:\Users\PcBoyarin\Desktop\GayDev\DmLab21-24\DmLab19.2\result\#1.txt");
                 for (int i = 0; i < 6; i++)
@@ -71,13 +88,43 @@ namespace DmLab19._2
                 }
                 fsr.Close();
             }
+
+            public void Zad2()
+            {
+                StreamWriter fsr = new StreamWriter(@"C:\Users\PcBoyarin\Desktop\GayDev\DmLab21-24\DmLab19.2\result\#2.txt");
+                word.Clear();
+                for (int i = 0; i < 6; i++)
+                    word.Add(alf[0]);
+                while (true)
+                {
+                    int j = 4;
+                    while (j >= 0 && word[j] == alf[5]) j--;
+                    if (j < 0) break;
+                    if (alf.IndexOf(word[j]) >= 6)
+                        j--;
+                    word[j] = alf[alf.IndexOf(word[j]) + 1];
+                    if (j == 4)
+                        if (BezPiAA())
+                        {
+                            Vivod(fsr);
+                            continue;
+                        }
+                    for (int k = j + 1; k < 5; k++)
+                        word[k] = alf[0];
+                    if (BezPiAA())
+                        Vivod(fsr);
+                }
+                fsr.Close();
+            }
+            
         }
             
         static void Main(string[] args)
         {
             string s = "a b c d e f";
             Object word = new Object(s);
-            word.zad1();
+            word.Zad1();
+            word.Zad2();
             Console.ReadKey();
         }
     }
